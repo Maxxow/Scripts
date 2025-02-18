0b9a909c6284692d9ab0f90245ef72849b54de7c
Maxxow <maximiliano.mtnz.gnlz@gmail.com>
2025-02-18 16:45:01 -0600

    programa2

---
+++ TallerDeSistemasOperativos/programa2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Ingresa el primer número:"
+read num1
+echo "Ingresa el segundo número:"
+read num2
+resultado=$((num1 * num2))
+echo "El resultado de multiplicar $num1 y $num2 es: $resultado"
