466ed50b6d6ee836e7bd5529fb54a502a75c52e9
Maxxow <maximiliano.mtnz.gnlz@gmail.com>
2025-02-28 16:49:52 -0600

    programa004

---
+++ TallerDeSistemasOperativos/programa004.sh
@@ -0,0 +1,14 @@
+a=1
+while [ $a -le 10 ]
+do
+	b=1
+	while [ $b -le 10 ]
+	do
+	r=`expr $b \* $a `
+	echo $a "*" $b "=" $r
+	b=`expr $b + 1 `
+	done
+	echo "Pulsa enter por favor"
+	read z clear
+	a=`expr $a \+ 1`
+done
